73a8c84a8213a51f56e3b22985a348d7c33751b6
unknown <cleytonw11@gmail.com>
2025-03-12 02:38:34 -0300

    added new exercise and created classes and methods

---
+++ java-poo/src/Alimento.java
@@ -0,0 +1,29 @@
+public class Alimento extends ItemDeLoja {
+
+    private Integer selo;
+    
+    public Alimento() {
+
+    }
+
+    public Alimento(Integer selo) {
+        this.selo = selo;
+    }
+
+    public Alimento(int codigoDoItem, String nomeDoItem, String descricaoDoItem, double valorDoItem, Integer selo) {
+        super(codigoDoItem, nomeDoItem, descricaoDoItem, valorDoItem);
+        this.selo = selo;
+    }
+
+    public Integer getSelo() {
+        return selo;
+    }
+
+    public void setSelo(Integer selo) {
+        this.selo = selo;
+    }
+    
+    public int getIdentificador() {
+        return selo;
+    }
+}
---
+++ java-poo/src/Ferramenta.java
@@ -0,0 +1,43 @@
+public class Ferramenta extends ItemDeLoja {
+
+    private String categoria;
+    private Integer serial;
+
+    public Ferramenta() {
+
+    }
+
+    public Ferramenta(String categoria, Integer serial) {
+        this.categoria = categoria;
+        this.serial = serial;
+    }
+
+    public Ferramenta(int codigoDoItem, String nomeDoItem, String descricaoDoItem, double valorDoItem, String categoria,
+            Integer serial) {
+        super(codigoDoItem, nomeDoItem, descricaoDoItem, valorDoItem);
+        this.categoria = categoria;
+        this.serial = serial;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getSerial() {
+        return serial;
+    }
+
+    public void setSerial(Integer serial) {
+        this.serial = serial;
+    }
+
+    @Override
+    public int getIdentificador() {
+        return serial;
+    }
+
+}
