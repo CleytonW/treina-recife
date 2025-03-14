fbf5c9b074dc26713f826c4de4bf7a89642d9884
unknown <cleytonw11@gmail.com>
2025-03-13 22:23:40 -0300

    finishing exercise

--- java-poo/src/exercicio07/Exercicio07.java
+++ java-poo/src/exercicio07/Exercicio07.java
@@ -2,6 +2,20 @@ package exercicio07;
 
 public class Exercicio07 {
     public static void main(String[] args) {
-        
+        Carro carro1 = new Carro();
+
+        carro1.setAnoModelo(2025);
+        carro1.setAnoFabricacao(2025);
+        carro1.setCor("Vermelho");
+
+        Modelo modelo1 = new Modelo();
+
+        modelo1.setMarca("Chevrolet");
+        modelo1.setNome("Onix");
+
+        Item item1 = new Item();
+
+        item1.setCodigo(159);
+        item1.setNome("Banco");
     }
 }
--- java-poo/src/exercicio07/Item.java
+++ java-poo/src/exercicio07/Item.java
@@ -1,5 +1,33 @@
 package exercicio07;
 
-public class Item {
-    
+public class Item extends Modelo {
+
+    private int codigo;
+    private String nome;
+    private String descricao;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
 }
